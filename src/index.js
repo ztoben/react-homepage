52a7b8db7ea24101bc2fb9184012f09289db15c7
Zach Toben <zach.toben@gmail.com>
2019-03-06 08:01:57 -0600

    fix style path

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Resume from './components/Resume';
 import Contact from './components/Contact';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './style/index.scss';
+import './style/Index.scss';
 
 const navItems = ['About', 'Resume', 'Contact'];
 
